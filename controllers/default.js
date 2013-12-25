3f2e85d338ea372622904ff6c9b61caae924cb3e
petersirka <petersirka@gmail.com>
2013-12-25 12:22:35 +0100

    added: stats

--- controllers/default.js
+++ controllers/default.js
@@ -1,3 +1,6 @@
+var stats = { upload: 0, read: 0, remove: 0 };
+var names = {};
+
 exports.install = function(framework) {
 	framework.route('/', plain_homepage);
 	framework.route('/{name}/', upload_files, ['put', 'upload'], (1024 * 1024) * 20); // 20 MB
@@ -5,12 +8,28 @@ exports.install = function(framework) {
 	framework.route('/{name}/{id}/', remove_file, ['delete']);
 	framework.route('/{name}/info/{id}/', json_file_info);
 	framework.route('/{name}/listing/', json_listing);
-	framework.route('/{name}/count/', json_count);	
+	framework.route('/{name}/count/', json_count);
 };
 
 function plain_homepage() {
 	var self = this;
-	self.plain(self.config.name + ' v' + self.config.version);
+	var builder = [];
+
+	builder.push('Pending requests          : ' + self.framework.stats.request.pending);
+	builder.push('Uploaded files            : ' + stats.upload);
+	builder.push('Read files                : ' + stats.read);
+	builder.push('Removed files             : ' + stats.remove);
+
+	builder.push('');
+	builder.push('Requests')
+	builder.push('========');
+	builder.push('');
+
+	Object.keys(names).forEach(function(key) {
+		builder.push('#' + key.padRight(25) + ': ' + names[key]);
+	});
+
+	self.plain(self.config.name + ' v' + self.config.version + '\n================\n\n' + builder.join('\n'));
 }
 
 function upload_files(name) {
@@ -20,9 +39,12 @@ function upload_files(name) {
 	var storage = self.filestorage(name || 'default');
 	var result = {};
 
+	names[name] = (names[name] || 0) + 1;
+
 	for (var i = 0; i < length; i++) {
 		var file = self.files[i];
 		result[file.name] = { id: storage.insert(file.filename, file.path), filename: file.filename };
+		stats.upload++;
 	}
 
 	self.json(result);
@@ -30,22 +52,27 @@ function upload_files(name) {
 
 function read_file(name, id) {
 	var self = this;
+	names[name] = (names[name] || 0) + 1;
 	self.custom();
+	stats.read++;
 	self.filestorage(name).pipe(id.parseInt(), self.req, self.res, true);
 }
 
 function remove_file(name, id) {
 	var self = this;
-
 	var arr = id.split('-');
 
+	names[name] = (names[name] || 0) + 1;
+
 	if (arr.length === 1) {
+		stats.remove++;
 		self.filestorage(name).remove(id.parseInt());
 		self.json({ success: true });
 		return;
 	}
 
 	arr.waiting(function(id, next) {
+		stats.remove++;
 		self.filestorage(name).remove(id.parseInt(), next);
 	});
 
@@ -55,6 +82,9 @@ function remove_file(name, id) {
 function json_listing(name) {
 
 	var self = this;
+
+	names[name] = (names[name] || 0) + 1;
+
 	self.filestorage(name).listing(function(err, arr) {
 
 		if (err) {
@@ -68,11 +98,13 @@ function json_listing(name) {
 
 function json_count(name) {
 	var self = this;
+	names[name] = (names[name] || 0) + 1;
 	self.json(self.filestorage(name).options);
 }
 
 function json_file_info(name, id) {
 	var self = this;
+	names[name] = (names[name] || 0) + 1;
 	self.filestorage(name).stat(id, function(err, header) {
 
 		if (err) {
